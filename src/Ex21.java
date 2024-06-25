94e6179c810c6660473998bc1dd2a961d1635fdc
yangjihun <ik5334@naver.com>
2024-06-25 20:03:12 +0900

    배열 예제

---
+++ src/Ex21.java
@@ -0,0 +1,11 @@
+public class Ex21 {
+    public static void main(String[] args) {
+        int [][]arr = new int[][] {{45,50,75}, {89}}; //2차원 배열
+        System.out.println(arr[0].length); // 배열의 0번째 크기 3 배열이 들어감
+        System.out.println(arr[1].length); // 배열 0번째의 크기 1 배열 길이 1
+        System.out.println(arr[0][0]); //배열의 0번째 0 값
+        System.out.println(arr[0][1]); //배열의 0번째 1값
+        System.out.println(arr[0][2]); //배열의 0 번째 2값
+        System.out.println(arr[1][0]); //배열의 1번째 0값
+    }
+}
