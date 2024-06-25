f6b6b62c1b955e231e28890f8bcd6041287c4271
yangjihun <ik5334@naver.com>
2024-06-25 20:34:14 +0900

    에러찾는 예제

---
+++ src/Ex23.java
@@ -0,0 +1,22 @@
+//Person ==========================
+class  Person {
+    private  String name;  //Person 클래스의 멤버 변수
+    public Person(String val){
+        name = val;
+    }
+    public String get() { // Person클래스의 static 메소드  에러코드:public static String get();
+        return  name;
+
+         }
+         public  void print() {
+             System.out.println(name); //name 출력
+         }
+}
+
+//Person ==========================
+public class Ex23 {
+    public static void main(String[] args) {
+    Person obj = new Person("Kim");
+    obj.print();
+    }
+}
