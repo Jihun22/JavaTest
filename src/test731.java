1407347528c0e25b4002ee9cdf0531169c5d9b6d
YANGJIHUN <ik5334@naver.com>
2024-07-31 11:24:43 +0900

    백준 테스트

---
+++ src/test731.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class test731 {
+    public static void main(String[] args) {
+//백준 테스트
+        Scanner sc  = new Scanner(System.in);
+        int a, b;
+        a= sc.nextInt();
+        b= sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
+}
+// 백준  프로그래밍 코드
+//import java.util.*;
+//public class Main{
+//	public static void main(String args[]){
+//		Scanner sc = new Scanner(System.in);
+//		int a, b;
+//		a = sc.nextInt();
+//		b = sc.nextInt();
+//		System.out.println(a - b);
+//	}
+//}
