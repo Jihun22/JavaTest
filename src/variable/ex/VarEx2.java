c92c3cca0e04b66ee34737c68b203268a69e70e0
YANGJIHUN <ik5334@naver.com>
2024-12-24 20:38:13 +0900

    김영한 자바 문제 2

---
+++ src/variable/ex/VarEx2.java
@@ -0,0 +1,21 @@
+package variable.ex;
+
+public class VarEx2 {
+
+    /*
+    1. 변수에 num1 을 선언하고 이에 10 할당함
+    2. 변수 num2를 선언하고 이에 20을 할당함
+    3.두 변수의 합을 구하고 그 결과를 새로운 변수 sum에 저장함
+    4.sum 변수의 값을 출력함
+
+     */
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+
+    }
+}
