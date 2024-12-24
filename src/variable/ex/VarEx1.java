1c1112e83457eb48dd8aaa5c14f975d1a990b777
YANGJIHUN <ik5334@naver.com>
2024-12-24 20:35:06 +0900

    김영한 자바 문제 1

---
+++ src/variable/ex/VarEx1.java
@@ -0,0 +1,24 @@
+package variable.ex;
+
+public class VarEx1 {
+    /**
+     *다음 코드에  반복해서 나오는 숫자 4, 3 을 다른 숫자로 한번에 변경할수 있도록 다음 변수 Num1, num2 변경해보기
+     */
+
+    public static void main(String[] args) {
+
+
+/*
+    System.out.println(4+3);
+        System.out.println(4 -3);
+        System.out.println(4*3);
+
+ */
+        int num1 = 4;
+        int num2 = 3;
+        System.out.println(num1+num2);
+        System.out.println(num1 -num2);
+        System.out.println(num1*num2);
+
+    }
+}
