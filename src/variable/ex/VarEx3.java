3e361e46fa59306d4c76b70831a8d9695517c6bd
YANGJIHUN <ik5334@naver.com>
2024-12-24 21:16:27 +0900

    김영한 자바 문제 3

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,15 @@
+package variable.ex;
+
+public class VarEx3 {
+    /*
+    long 타입의 변수 선언 하고 그변수 백억 으로 초기화 후 출력하는 프로그램 작성
+    boolean 타입의 변수를 선언하고 그 변수를 true 로 초기화한 후 출력하는 프로그램 작성
+     */
+    public static void main(String[] args) {
+
+        long s= 100000000L;
+        System.out.println(s);
+        boolean booleans =true ;
+        System.out.println(booleans);
+    }
+}
